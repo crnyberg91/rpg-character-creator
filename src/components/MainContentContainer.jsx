c50e44ce42bc944ebab63f4f0050fe97cc2971a3
Chris Nyberg <chrisn83@gmail.com>
2023-02-02 14:50:46 -0500

    MainContentContainer Component created

---
+++ src/components/MainContentContainer.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const MainContentContainer = () => {
+	return (
+		<div className='bg-black mx auto h-screen px-4 flex items-center w-full'>
+			<h1 className='text-5xl font-signature ml-2'>
+				LOREM IPSUMED
+			</h1>
+		</div>
+	);
+};
+
+export default MainContentContainer;
